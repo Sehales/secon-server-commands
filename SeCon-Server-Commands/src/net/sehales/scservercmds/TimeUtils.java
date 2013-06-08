665eba97b90f172fe446e25169c306af6025d2b1
sehales <jan@sehales.net>
2013-06-09 01:45:18 +0200

    remade timeutils

--- SeCon-Server-Commands/src/net/sehales/scservercmds/TimeUtils.java
+++ SeCon-Server-Commands/src/net/sehales/scservercmds/TimeUtils.java
@@ -18,60 +18,46 @@ public class TimeUtils {
 
 		Pattern timePattern = Pattern.compile(
 
-		"(?:([0-9]+)\\s*y[a-z]*[,\\s]*)?" +
+		"(?:([0-9]+)y[a-z]*[,]*)?" +
 
-		"(?:([0-9]+)\\s*mo[a-z]*[,\\s]*)?" +
+		"(?:([0-9]+)mo[a-z]*[,]*)?" +
 
-		"(?:([0-9]+)\\s*w[a-z]*[,\\s]*)?" +
+		"(?:([0-9]+)w[a-z]*[,]*)?" +
 
-		"(?:([0-9]+)\\s*d[a-z]*[,\\s]*)?" +
+		"(?:([0-9]+)d[a-z]*[,]*)?" +
 
-		"(?:([0-9]+)\\s*h[a-z]*[,\\s]*)?" +
+		"(?:([0-9]+)h[a-z]*[,]*)?" +
 
-		"(?:([0-9]+)\\s*m[a-z]*[,\\s]*)?" +
+		"(?:([0-9]+)m[a-z]*[,]*)?" +
 
-		"(?:([0-9]+)\\s*(?:s[a-z]*)?)?",
+		"(?:([0-9]+)(?:s[a-z]*)?)?",
 
 		Pattern.CASE_INSENSITIVE);
 
 		Matcher m = timePattern.matcher(time);
 
-		int years = 0;
-		int months = 0;
-		int weeks = 0;
-		int days = 0;
-		int hours = 0;
-		int minutes = 0;
-		int seconds = 0;
-		boolean found = false;
-		while (m.find()) {
-			if (m.group() == null || m.group().isEmpty())
-				continue;
-			for (int i = 0; i < m.groupCount(); i++)
-				if (m.group(i) != null && !m.group(i).isEmpty()) {
-					found = true;
-					break;
-				}
-			if (found) {
-				if (m.group(1) != null && !m.group(1).isEmpty())
-					years = Integer.parseInt(m.group(1));
-				if (m.group(2) != null && !m.group(2).isEmpty())
-					months = Integer.parseInt(m.group(2));
-				if (m.group(3) != null && !m.group(3).isEmpty())
-					weeks = Integer.parseInt(m.group(3));
-				if (m.group(4) != null && !m.group(4).isEmpty())
-					days = Integer.parseInt(m.group(4));
-				if (m.group(5) != null && !m.group(5).isEmpty())
-					hours = Integer.parseInt(m.group(5));
-				if (m.group(6) != null && !m.group(6).isEmpty())
-					minutes = Integer.parseInt(m.group(6));
-				if (m.group(7) != null && !m.group(7).isEmpty())
-					seconds = Integer.parseInt(m.group(7));
-				break;
-			}
-		}
-		if (!found)
-			throw new Exception("Wrong input");
+		int years = 0, months = 0, weeks = 0, days = 0, hours = 0, minutes = 0, seconds = 0;
+
+		m.find();
+
+		if (m.group() == null || m.group().isEmpty())
+			throw new Exception("Illegal input");
+
+		if (m.group(1) != null && !m.group(1).isEmpty())
+			years = Integer.parseInt(m.group(1));
+		if (m.group(2) != null && !m.group(2).isEmpty())
+			months = Integer.parseInt(m.group(2));
+		if (m.group(3) != null && !m.group(3).isEmpty())
+			weeks = Integer.parseInt(m.group(3));
+		if (m.group(4) != null && !m.group(4).isEmpty())
+			days = Integer.parseInt(m.group(4));
+		if (m.group(5) != null && !m.group(5).isEmpty())
+			hours = Integer.parseInt(m.group(5));
+		if (m.group(6) != null && !m.group(6).isEmpty())
+			minutes = Integer.parseInt(m.group(6));
+		if (m.group(7) != null && !m.group(7).isEmpty())
+			seconds = Integer.parseInt(m.group(7));
+
 		Calendar c = new GregorianCalendar();
 		if (years > 0)
 			c.add(Calendar.YEAR, years);
